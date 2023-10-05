7b707a0b5ec3e7788279a1f8aee7767802844eba
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-10-04 23:46:25 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "util.h"
+
+const int width = 800;
+const int height = 600;
+
+void framebuffer_resize(GLFWwindow* window, int new_width, int new_height)
+{
+    glViewport(0, 0, new_width, new_height);
+}
+
+int main()
+{
+    // initialize and configure GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create window and make context current
+    GLFWwindow* window = glfwCreateWindow(width, height, "Intro to OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        // throw exception
+        std::cout << "GLFW Error" << std::endl;
+    }
+    glfwMakeContextCurrent(window);
+
+    // load address of OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+          std::cout << "GLAD Error" << std::endl;
+    }
+
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize);
+
+    int shader_program = create_program("", "");
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
